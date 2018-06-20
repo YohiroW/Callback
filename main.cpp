44c1005b10a0fcadfa4e39d9648176428e18fe12
YohiroW <tz281678310@sina.com>
2018-06-20 21:53:14 +0800

    Added test case for FuncRT0

--- main.cpp
+++ main.cpp
@@ -14,15 +14,26 @@ public:
 		Func0::MakeFunc(this, &TestCase::LogFunc0).Call();
 	}
 
+	void TestFuncRT0()
+	{
+		FuncRT0<float> functorRT = FuncRT0<float>::MakeFunc(this, &TestCase::LogFuncRT0);
+		std::cout<< "Test Member FuncRT0, value: "<< functorRT.Call()<< std::endl;
+	}
 
 private:
 	void LogFunc0()
 	{
 		std::cout << "Test Member Func0" << std::endl;
 	}
+
+	float LogFuncRT0()
+	{
+		return 66.6f;
+	}
 };
 
 void test_func0() { std::cout << "Test Func0" << std::endl; }
+int  test_funcRT0() { return 23; }
 
 //Entry
 int main()
@@ -30,8 +41,12 @@ int main()
 	Func0 ftor0 = Func0::MakeFunc(test_func0);
 	ftor0.Call();
 
+	FuncRT0<int> ftorRT = FuncRT0<int>::MakeFunc(test_funcRT0);
+	std::cout<< "Test FuncRT0, value:"<< ftorRT.Call()<< std::endl;
+
 	TestCase test;
 	test.TestFunc0();
+	test.TestFuncRT0();
 
 	return 0;
 }
